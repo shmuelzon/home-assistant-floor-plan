4d3710884004e15b31b975137dbeb50c1b976326
DigiLive <github@digilive.nl>
2024-12-27 10:32:38 +0100

    Refactor state-icon radius constant
    * A typo was present in the name of the constant.
    * Inspecting a state-icon in the browser reveals the width of the icon
      is actually 40 px. The value of the constant is changed to reflect
      this width.

--- src/com/shmuelzon/HomeAssistantFloorPlan/Controller.java
+++ src/com/shmuelzon/HomeAssistantFloorPlan/Controller.java
@@ -890,11 +890,11 @@ public class Controller {
     }
 
     private boolean doStateIconsIntersect(Entity first, Entity second) {
-        final double STATE_ICON_RAIDUS_INCLUDING_MARGIN = 25.0;
+        final double STATE_ICON_RADIUS_INCLUDING_MARGIN = 20.0;
 
         double x = Math.pow(first.position.x - second.position.x, 2) + Math.pow(first.position.y - second.position.y, 2);
 
-        return x <= Math.pow(STATE_ICON_RAIDUS_INCLUDING_MARGIN * 2, 2);
+        return x <= Math.pow(STATE_ICON_RADIUS_INCLUDING_MARGIN * 2, 2);
     }
 
     private boolean doesStateIconIntersectWithSet(Entity entity, Set<Entity> entities) {
